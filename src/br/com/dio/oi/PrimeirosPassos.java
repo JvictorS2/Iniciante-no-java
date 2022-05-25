3fc5a9060463933d68659b2f8e980e841a31e440
JvitorS2 <vssimoesdunck@gmail.com>
2022-05-24 23:12:54 -0300

    Send commit

---
+++ src/br/com/dio/oi/PrimeirosPassos.java
@@ -0,0 +1,27 @@
+package br.com.dio.oi;
+
+public class PrimeirosPassos {
+    public static void main(String[] args) {
+
+        int i = 0;
+        //int i;
+        int I = 9;
+        // int 1a;
+        int _1a = 9;
+        int $aq = 8;
+        final int j = 10;
+        //j = 15;
+        int k = 5;
+        int quantidadeProdutos = 50;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(quantidadeProdutos);
+        System.out.println(i);
+
+    }
+}
---
+++ src/br/com/dio/oi/tiposdedados.java
@@ -0,0 +1,39 @@
+package br.com.dio.oi;
+
+public class tiposdedados {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000; muito grande
+
+        //int i1 = -10000000000; muito grande/pequeno
+        int i2 = 1000000000;
+
+        long l1 = 1000000000000000000L;
+        //long l2 = 1000000000000000000; falta o "L"
+
+        float f1 = 4.5f;
+        //float f2 = 4.5; flata "f"
+
+        double d1 = 85.49;
+        double d2 = 4.5;
+
+        char c1 = 'c';
+        //char c2 = 'cw'; apenas um caracter.
+
+        boolean b3 = false;
+        boolean b4 = true;
+
+
+
+
+
+
+
+
+
+    }
+}
